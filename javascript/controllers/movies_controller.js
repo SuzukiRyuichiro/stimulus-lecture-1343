a9196b075efd3dd00adcbbb0b314357cd0f8a697
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-10 08:55:44 +0100

    value

--- javascript/controllers/movies_controller.js
+++ javascript/controllers/movies_controller.js
@@ -8,25 +8,31 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
   static targets = ["input", "results"];
+  static values = { baseUrl: String };
 
   connect() {
     console.log("movies controller connected");
+    console.log(this.baseUrlValue);
+  }
+
+  fetchMovies(query) {
+    fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+      .then((response) => response.json())
+      .then((data) => this.insertMovies(data));
+  }
+
+  insertMovies(data) {
+    data.Search.forEach((result) => {
+      const movieTag = `<li class="list-group-item border-0">
+      <img src="${result.Poster}" alt="" width="100" height="100%">
+    </li>`;
+      this.resultsTarget.insertAdjacentHTML("beforeend", movieTag);
+    });
   }
 
   search(event) {
-    console.log(this.inputTarget);
     event.preventDefault();
-    fetch(
-      `http://www.omdbapi.com/?s=${this.inputTarget.value}&i=tt3896198&apikey=2b73eb94`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        data.Search.forEach((result) => {
-          const movieTag = `<li class="list-group-item border-0">
-        <img src="${result.Poster}" alt="" width="100" height="100%">
-      </li>`;
-          this.resultsTarget.insertAdjacentHTML("beforeend", movieTag);
-        });
-      });
+    this.resultsTarget.innerHTML = "";
+    this.fetchMovies(this.inputTarget.value);
   }
 }
